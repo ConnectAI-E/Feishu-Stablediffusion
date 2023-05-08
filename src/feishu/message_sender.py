fb85af39d41850bad4bbf3044d06d34d6e4add5a
Hank <hanckjt@gmail.com>
2023-05-08 12:34:26 +0800

    修改消息已回复的方式输出

--- src/feishu/message_sender.py
+++ src/feishu/message_sender.py
@@ -2,8 +2,10 @@ import json
 import time
 import attr
 from larksuiteoapi.api import Request, set_timeout
+from larksuiteoapi.service.im.v1 import Service as ImService, model
 from larksuiteoapi import Config, ACCESS_TOKEN_TYPE_TENANT, DOMAIN_FEISHU
 from util.app_config import app_config
+from feishu.feishu_conf import feishu_conf
 from util.logger import app_logger
 
 @attr.s
@@ -17,16 +19,20 @@ class MessageSender:
             raise Exception("conf is required")
         self.conf = conf
 
-    def send_text_message(self, chat_id, msg, append=True):
+    def send_text_message(self, chat_id, user_id, msg_id, msg):
         body = {
             "chat_id": chat_id,
             "msg_type": "text",
-            "content": {
-                "text": msg
-            }
+            "content": json.dumps({"text": f'<at user_id="{user_id}"></at> {msg}'}),
         }
-        req = Request('/open-apis/message/v4/send', 'POST', ACCESS_TOKEN_TYPE_TENANT, body,
-                      output_class=Message, request_opts=[set_timeout(3)])
+        req = Request(
+            f"/open-apis/im/v1/messages/{msg_id}/reply",
+            "POST",
+            ACCESS_TOKEN_TYPE_TENANT,
+            body,
+            output_class=Message,
+            request_opts=[set_timeout(3)],
+        )
         resp = req.do(self.conf)
         app_logger.debug("send_text_message:%s", msg)
         if resp.code == 0:
@@ -34,45 +40,45 @@ class MessageSender:
             return True
         else:
             app_logger.error(
-                "send message failed, code:%s, msg:%s, error:%s", resp.code, resp.msg, resp.error)
+                "send message failed, code:%s, msg:%s, error:%s",
+                resp.code,
+                resp.msg,
+                resp.error,
+            )
             return False
+
     # 发送消息卡片
-    def send_message_card(self, chat_id, messageCard):
+    def send_message_card(self, chat_id, user_id, msg_id, messageCard):
         body = {
             "chat_id": chat_id,
+            "user_id": user_id,
+            "root_id": msg_id,
             "msg_type": "interactive",
-            "card": messageCard
+            "card": messageCard,
         }
-        req = Request('/open-apis/message/v4/send', 'POST', ACCESS_TOKEN_TYPE_TENANT, body,
-                      output_class=Message, request_opts=[set_timeout(3)])
+        req = Request(
+            "/open-apis/message/v4/send",
+            "POST",
+            ACCESS_TOKEN_TYPE_TENANT,
+            body,
+            output_class=Message,
+            request_opts=[set_timeout(3)],
+        )
         resp = req.do(self.conf)
         app_logger.debug("send_command_card to %s", chat_id)
         if resp.code == 0:
             return True
         else:
             app_logger.error(
-                "send message failed, code:%s, msg:%s, error:%s", resp.code, resp.msg, resp.error)
-            return False
-
-    def send_card(self, chat_id, card):
-        body = {
-            "chat_id": chat_id,
-            "msg_type": "interactive",
-            "card": card
-        }
-        req = Request('/open-apis/message/v4/send', 'POST', ACCESS_TOKEN_TYPE_TENANT, body,
-                      output_class=Message, request_opts=[set_timeout(3)])
-        resp = req.do(self.conf)
-        app_logger.debug("send_command_card to %s", chat_id)
-        if resp.code == 0:
-            return True
-        else:
-            app_logger.error(
-                "send message failed, code:%s, msg:%s, error:%s", resp.code, resp.msg, resp.error)
+                "send message failed, code:%s, msg:%s, error:%s",
+                resp.code,
+                resp.msg,
+                resp.error,
+            )
             return False
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app_config.validate()
     app_settings = Config.new_internal_app_settings_from_env()
     conf = Config(DOMAIN_FEISHU, app_settings)
