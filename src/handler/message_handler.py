3b901da45d6ee5ece86d766c277ef8dd22413a41
Hank <hanckjt@gmail.com>
2023-05-12 02:07:47 +0800

    添加负词支持，用"#"分割正负词。

--- src/handler/message_handler.py
+++ src/handler/message_handler.py
@@ -2,25 +2,16 @@ import json
 
 from larksuiteoapi import Config
 
-from feishu.message_sender import MessageSender
+from feishu.message_sender import message_sender
 from feishu.upload_image import upload_image
-from util.app_config import AppConfig
-from util.logger import app_logger
-from util.event_helper import get_pure_message
 from service.generate_config import GenerateConfig
 from feishu.message_card import handle_image_card
 from service.stablediffusion import sd_webui
-
+from util.event_helper import MyReceiveEvent
 
 class MessageHandler:
-    def __init__(self, app_config: AppConfig, conf: Config):
-        if not app_config:
-            raise Exception("app_config is required")
-        if not conf:
-            raise Exception("conf is required")
-        self.conf = conf
-        self.app_config = app_config
-        self.message_sender = MessageSender(self.conf)
+    def __init__(self):
+        pass
 
     def parse_command_line_args(self, command_line_args):
         result = {}
@@ -55,27 +46,30 @@ class MessageHandler:
     def handle_update_message_card(self, token, openId, prompt):
         messageCard = self.handle_prompt(prompt)
         messageCard["open_ids"] = [openId]
-        return self.message_sender.update_message_card(token, messageCard)
+        return message_sender.update_message_card(token, messageCard)
 
     # 根据指令生成不同的消息卡片
     def handle_prompt(self, prompt):
         inputModel = self.parse_command_line_args(prompt)
+
+        # Check if the prompt contains the substring "#" as the negative prompt
+        prompts = inputModel["prompt"].split('#', 1)
+        if len(prompts) > 1:
+            inputModel['prompt'] = prompts[0]
+            inputModel['negative_prompt'] = prompts[1]
+
         # Check if the prompt contains the substring "/help"
-        image_cfg = GenerateConfig()
-        image_cfg.update_image_configuration(inputModel, image_cfg)
-        image_configuration = image_cfg.get_config_json()
+        gen_cfg = GenerateConfig()
+        gen_cfg.update_configuration(inputModel, gen_cfg)
+        image_configuration = gen_cfg.get_config_json()
         images_json = sd_webui.generate_images(image_configuration)
         images_key = []
         for img_data in images_json['images']:
             images_key.append(upload_image(img_data))
         return handle_image_card(images_json['info'], images_key)
 
-    def handle_message(self, event):
-        user_id = event.event.sender.sender_id.user_id
-        chat_id = event.event.message.chat_id
-        msg_id = event.event.message.message_id
-        
-        text = get_pure_message(event)
-        messageCard = self.handle_prompt(text)
+    def handle_message(self, myevent: MyReceiveEvent):
+        message_sender.send_text_message(myevent, f"正在生成图片，{sd_webui.queue()}")
+        messageCard = self.handle_prompt(myevent.text)
 
-        return self.message_sender.send_message_card(chat_id, user_id, msg_id, messageCard)
+        return message_sender.send_message_card(myevent, messageCard)
