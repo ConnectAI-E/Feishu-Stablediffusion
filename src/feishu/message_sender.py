fbe46bdb2f23475136308139fe0d5c0fd659df4a
Hank <hanckjt@gmail.com>
2023-05-16 23:34:44 +0800

    修复text解析出错的BUG

--- src/feishu/message_sender.py
+++ src/feishu/message_sender.py
@@ -7,6 +7,7 @@ from util.event_helper import MyReceiveEvent
 from feishu.feishu_conf import feishu_conf
 from util.logger import app_logger
 
+
 @attr.s
 class Message(object):
     message_id = attr.ib(type=str)  # type: ignore
@@ -18,20 +19,17 @@ class MessageSender:
             raise Exception("conf is required")
         self.conf = conf
 
-    def send_text_message(self, myevent: MyReceiveEvent, text, mention_user = False):
+    def send_text_message(self, myevent: MyReceiveEvent, text, mention_user=False):
         chat_id = myevent.get_chat_id()
         user_id = myevent.get_user_id()
         msg_id = myevent.get_message_id()
-        
-        body = {
-            "chat_id": chat_id,
-            "msg_type": "text"
-        }
+
+        body = {"chat_id": chat_id, "msg_type": "text"}
         if mention_user:
             body["content"] = json.dumps({"text": f'<at user_id="{user_id}"></at> \n{text}'})
         else:
-            body["content"] = json.dumps({"text": text})
-        
+            body["content"] = json.dumps({"text": f'{text}'})
+
         req = Request(
             f"/open-apis/im/v1/messages/{msg_id}/reply",
             "POST",
@@ -86,7 +84,7 @@ class MessageSender:
                 resp.error,
             )
             return False
-        
+
     # 更新消息卡片
     def update_message_card(self, token, messageCard):
         body = {
@@ -113,5 +111,6 @@ class MessageSender:
                 resp.error,
             )
             return False
+
 
 message_sender = MessageSender(feishu_conf)
