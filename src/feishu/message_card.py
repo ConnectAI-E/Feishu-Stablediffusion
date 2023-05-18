93cc193b93844f12c00ccc8bf6f0d375d51ac2a4
yuchengli <244065684@qq.com>
2023-05-18 11:23:57 +0800

    fix: 格式化 infotexts 结果

--- src/feishu/message_card.py
+++ src/feishu/message_card.py
@@ -32,22 +32,46 @@ def handle_list_info_card(LIST_INFO_CARD, list):
         LIST_INFO_CARD["elements"].append(element)
     return LIST_INFO_CARD
 
-def format_input_str(input_str, str_arr):
-    for key in str_arr:
-        input_str = input_str.replace(key + ':', '\n **【' + key + '】** ')
-    return input_str
+def handle_infotexts(obj):
+    # 获取 Model 信息并将其作为一个新的字段添加到对象中
+    model_info = ''
+    for text in obj['infotexts']:
+        if 'Model: ' in text:
+            model_info = text.split('Model: ')[1].split(',')[0]
+    del obj['infotexts']
+    del obj['all_prompts']
+    del obj['all_negative_prompts']
+    del obj['all_seeds']
+    del obj['all_subseeds']
+    del obj['subseed']
+    del obj['subseed_strength']
+    del obj['seed_resize_from_w']
+    del obj['seed_resize_from_h']
+    del obj['job_timestamp']
+    del obj['extra_generation_params']
+    del obj['is_using_inpainting_conditioning']
+    del obj['index_of_first_image']
+
+    obj['model'] = [model_info]
+
+    def format_input_str(input_str, str_arr):
+      for key in str_arr:
+          input_str = input_str.replace(key + ':', '\n **【' + key + '】** ')
+      return input_str
+  
+    # 格式化字符串
+    formatted_str = format_input_str('', obj.keys())
+    for key, value in obj.items():
+        formatted_str += '\n **【' + key + '】** ' + str(value)
+
+    return formatted_str
 
 def handle_image_card(image_info, img_key_list, prompt):
-    split_datas = []
-    for data in image_info['infotexts']:
-      split_datas.append('prompt: '+ data.replace('\n', ''))
-
     elements = [
         {"tag": "column_set", "flex_mode": "none", "background_style": "default", "columns": []},
     ]
 
     for index, img_key in enumerate(img_key_list):
-        print(split_datas[index])
         elements.append(
           {
                       "tag": "img",
@@ -62,7 +86,7 @@ def handle_image_card(image_info, img_key_list, prompt):
         elements.append(
             {
                   "tag": "markdown",
-                  "content": format_input_str(split_datas[index], str_arr),
+                  "content": handle_infotexts(image_info),
             }
         )
 
